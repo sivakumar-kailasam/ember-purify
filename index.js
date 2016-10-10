93cd99814e507e8e535548a09110c89402b6abf7
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-10-10 12:45:23 +0530

    Use eslint & import DOMPurify

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-/* jshint node: true */
+/* eslint node: true */
 'use strict';
+var path = require('path');
+
 
 module.exports = {
-  name: 'ember-purify'
+  name: 'ember-purify',
+
+  included: function included(app) {
+    var domPurifyScript = 'DOMPurify/src/purify.js';
+    if (app.env === 'production') {
+      domPurifyScript = 'DOMPurify/dist/purify.min.js';
+    }
+    app.import(path.join(app.bowerDirectory, domPurifyScript));
+  }
+
 };
